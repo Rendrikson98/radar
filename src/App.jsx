c0ea27174358430420b94b36501ec447a43ceae7
Rendrikson98 <rendrikson16@gmail.com>
2022-12-14 12:19:41 -0300

    initializing menu styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Radar } from './page/Radar';
 import './styles/global.scss';
 
--- src/page/Radar.tsx
+++ src/page/Radar.tsx
@@ -1,10 +1,10 @@
-import { Button, Spinner, Classes } from '@blueprintjs/core';
-import '../styles/repositories.scss';
+import React from 'react';
+import Menu from '../components/Menu/index';
+
 export const Radar = () => {
   return (
     <>
-      <h1>Radar</h1>
-      <Button className={Classes.LARGE}>teste</Button>
+      <Menu />
     </>
   );
 };
